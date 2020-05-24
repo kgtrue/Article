958a473d39e5aba6fba6ec83503b20e747aadcff
Kristian <True>
2020-05-24 21:27:23 +0200

    search

---
+++ src/Articles.Core.Application/Common/Interfaces/Contracts/IArticleSearchRepo.cs
@@ -0,0 +1,15 @@
+﻿using Articles.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Articles.Core.Application.Common.Interfaces.Contracts
+{
+    public interface IArticleSearchRepo
+    {
+        public Task<IEnumerable<Article>> Search(Expression<Func<Article, bool>> filter, CancellationToken cancellationToken);
+    }
+}
