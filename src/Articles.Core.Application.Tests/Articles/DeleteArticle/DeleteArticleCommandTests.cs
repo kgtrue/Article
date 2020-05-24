61ae5930cfc0c7fcca4e558bdd847bfc14a224be
Kristian <True>
2020-05-24 22:16:44 +0200

    tests

---
+++ src/Articles.Core.Application.Tests/Articles/DeleteArticle/DeleteArticleCommandTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Articles.Core.Application.Tests.Articles.DeleteArticle
+{
+    class DeleteArticleCommandTests
+    {
+    }
+}
